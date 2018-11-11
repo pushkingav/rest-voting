4b161045a03e0d984fd8a3db30da7eba230516b8
pushkingav <av.pushking@gmail.com>
2018-11-11 16:21:31 +0300

    Add spring-app.xml and main class for testing purposes

---
+++ src/main/java/ru/restaurants/restvoting/SpringMain.java
@@ -0,0 +1,15 @@
+package ru.restaurants.restvoting;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+
+public class SpringMain {
+    public static void main (String[] args) {
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+            System.out.println(Arrays.toString(appCtx.getBeanDefinitionNames()));
+        }
+
+    }
+}

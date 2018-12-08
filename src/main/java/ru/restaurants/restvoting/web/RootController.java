70088e7a51ce4e03dab5d2d39626a87ab4486e29
apushkin <apushkin@ipmks.com>
2018-12-08 17:09:38 +0300

    Add webjars. Fix tomcat by setting the "contextPath" in idea launch configuration

---
+++ src/main/java/ru/restaurants/restvoting/web/RootController.java
@@ -0,0 +1,12 @@
+package ru.restaurants.restvoting.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+}

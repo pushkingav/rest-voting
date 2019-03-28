efb7e0491a1247c4718a16d79375f13e25dccb58
apushkin <apushkin@ipmks.com>
2019-03-28 10:35:18 +0300

    Fix db population. Add menu items on db population.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,9 +1,9 @@
+DELETE FROM menu;
 DELETE FROM user_roles;
 DELETE FROM dishes;
 DELETE FROM users;
 DELETE FROM restaurants;
-DELETE FROM MENU;
-DELETE FROM votes;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -28,7 +28,21 @@ INSERT INTO dishes (description)
 VALUES ('Овсянка'), ('Омлет'), ('Салатик'), ('Стейк'),
        ('Гренки'), ('Борщ'), ('Цыпленок табака'), ('Пиво'), ('Тарелка овощная');
 
-INSERT INTO votes (user_id, date, restaurant_id) values (100000, NOW, 100005),
+INSERT INTO menu (restaurant_id, dish_id, date, price) VALUES (100005, 100008, NOW, 50),
+																															(100005, 100009, NOW, 75),
+																															(100005, 100010, NOW, 100),
+																															(100005, 100011, NOW, 500),
+																															(100006, 100012, NOW, 100),
+																															(100006, 100013, NOW, 80),
+																															(100006, 100014, NOW, 250),
+																															(100006, 100015, NOW, 150),
+																															(100007, 100016, NOW, 110),
+																															(100007, 100008, NOW, 75),
+																															(100007, 100009, NOW, 90),
+																															(100007, 100011, NOW, 650);
+
+
+INSERT INTO votes (user_id, date, restaurant_id) VALUES (100000, NOW, 100005),
 																												(100001, NOW, 100005),
 																												(100002, NOW, 100006),
 																												(100003, NOW, 100007),
